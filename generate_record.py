1ac1479159e41ad03c9563f2938b7ca009f3f3c4
Fayed <fayed@Fayeds-MacBook-Pro.local>
2023-11-20 16:04:27 -0500

    [synthetic-record-creation] fixing some bugs with record generation

--- generate_record.py
+++ generate_record.py
@@ -36,13 +36,14 @@ def save_records_to_file(records, dir):
 
 def synthesize_records_centor(seed_records, new_record_count, output_dir):
     #check token limits:
-    if (len(seed_records) + new_record_count) > 16:
+    n_seed_records = len(seed_records)
+    if (n_seed_records + new_record_count) > 16:
         print(len(seed_records))
         print(new_record_count)
         print("[ERROR] total token count will exceed token limit of model. Reduce number of seed records or lower number of output records.")
         return None
     
-    system_prompt = f"synthea creates synthetic but realistic EHR patient records. Given an example of such record create {new_record_count} other examples with different data but a similar chief complaint.\n\nMake sure to vary the following data:\n\n1. age\n2. temp\n3. tonsils condition\n4. lymph nodes condition\n5. cough presence\nSeparate records by the following string:\n####"
+    system_prompt = f"synthea creates synthetic but realistic EHR patient records. Given {n_seed_records} example{'' if n_seed_records == 1 else 's'} of such record create {new_record_count + n_seed_records} example{'' if new_record_count == 1 else 's'} with different data but a similar chief complaint.\n\nMake sure to vary the following data:\n\n1. age\n2. temp\n3. tonsils condition\n4. lymph nodes condition\n5. cough presence\End each record by the following string:\n####"
 
     messages=[{"role": "system", "content": system_prompt}]
 
@@ -54,7 +55,7 @@ def synthesize_records_centor(seed_records, new_record_count, output_dir):
             {"role": "user", "content": record}
         )
 
-    response = call_openai_chat(msgs=messages, mode=Mode.DEV)
+    response = call_openai_chat(msgs=messages, mode=Mode.DEBUG)
     
     output_records = response.split("####")
     save_records_to_file(output_records, output_dir)
