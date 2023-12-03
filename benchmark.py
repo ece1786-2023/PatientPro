4ae6777978b4d935c39a7f0397690f8bc84b2977
solimantwali <solimantwali@gmail.com>
2023-12-03 15:03:45 -0500

    testing functionality for benchmark + more examples
    * added create_template() function to benchmark to easily create
      template for labelle examples for evaluation
    * qsofa_labels.csv will be the labels we compare the output to
      (datatable)
    * added some new records in qSOFA/records. changes reflected in
      ICU_note_EDA.ipynb

--- benchmark.py
+++ benchmark.py
@@ -1,16 +1,25 @@
-import metrics
-from io_helper import read_file_list, get_dir_files
 import argparse
-import run_model
 import sys
 import os
 import json
 import pandas as pd
+import csv
+import metrics
+import run_model
+from helper.io_helper import read_file_list, get_dir_files
 
-
+def create_template(metric, input_dir_str):
+    headers = ["EHR"] + metric.extract_keys() + ["Score"]
+    files = get_dir_files(input_dir_str)
+    o_fn = metric.id + "_labels.csv"
+    with open(o_fn, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows([[file] + [""]*(len(headers)-1) for file in files])
+    print('created', o_fn)
 
 def benchmark(metric, input_dir_str, n_shots=0):
-    files = get_dir_files(input_dir_str)
+    files = get_dir_files(input_dir_str)[-1:]
     jsons = []
     scores = []
     
@@ -24,22 +33,21 @@ def benchmark(metric, input_dir_str, n_shots=0):
         scores.append(metric.compute_score(jsonski))
     df = pd.DataFrame(jsons)
     df.insert(0, 'EHR', files)
-    
     df['Score'] = scores
 
     return df
 
 
 def arg_error_check(args):
-    metrics = {"centor", "qsofa"}
+    metric_list = [c.id for c in metrics.Metric.__subclasses__()]
     if args.n_shots < 0 or args.n_shots > 3:
         print("[ERROR] n_shots must be an integer between 0 and 3.")
         sys.exit()
     elif not os.path.exists(args.input_dir):
         print(f"[ERROR] Directory not found: {args.input_dir}")
         sys.exit()
-    elif not args.metric in metrics:
-        print(f"[ERROR] invalid metric: {args.metric}. Please select one of the following: {metrics}")
+    elif not args.metric in metric_list:
+        print(f"[ERROR] invalid metric: {args.metric}. Please select one of the following: {metric_list}")
         sys.exit()
     return
 
@@ -57,10 +65,11 @@ def main():
     input_dir_str = args.input_dir
     metric = metrics.get_metric(metric_str)
     
+    create_template(metric, input_dir_str)
     df = benchmark(metric, input_dir_str, n_shots)
 
     print(df)
-    print(df.to_latex())
+    # print(df.to_latex())
         
 
 if __name__ == "__main__":
